4ac89ed7e72d486e46673f320da32fcca9b5324e
momulin <jles87006@gmail.com>
2018-04-15 16:20:20 +0800

    bot add

--- server/linebot/bot.js
+++ server/linebot/bot.js
@@ -1,5 +1,6 @@
 var linebot = require('linebot');
 var express = require('express');
+var request = require('request');
 
 var botrouter = express.Router();
 const app = express();
@@ -15,12 +16,22 @@ bot.on('message', function(event) {
   console.log(event);
 =======
 bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {
-    console.log(data);
-  }).catch(function (error) {
+  var text = event.message.text;
+request('./../api/find/'+text, function (error, response, body) {
+  if(!error){
+    event.reply(body.id).then(function (data) {
+      
+    }).catch(function (error) {
 
+    });
+  }
+});
+
+<<<<<<< HEAD:server/linebot/bot.js
   });
 >>>>>>> cca7acd... router edit:server/router/bot.js
+=======
+>>>>>>> b7b9034... bot add:server/router/bot.js
 });
 
 
