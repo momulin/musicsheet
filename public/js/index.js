29cf2a761fa9fd23cf42f5186133896448eaaac0
momulin <jles87006@gmail.com>
2018-04-15 16:31:46 +0800

    remake delete

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,25 @@
 
+var tableclass = 'table-info';
+
 var additem = function(items){
   var tr = $('<tr></tr>');
-  tr.attr('id',items._id);
-  var del = $("<button class='btn btn-outline-primary'>delete</button>");
-  del.attr('value',items._id).attr('id','delete');
-  del.click(deleteitem);
+  tr.attr('value',items._id);
+  // tr.attr('id',items._id);
+  // var del = $("<button class='btn btn-outline-primary'>delete</button>");
+  // del.attr('value',items._id).attr('id','delete');
+  // del.click(deleteitem);
   tr.append($("<td></td>").text(items.id));
   tr.append($("<td></td>").text(items.name));
   tr.append($("<td></td>").text(items.author));
+  tr.click(addTrClass);
   // tr.append(`<td>${items.id}</td><td>${items.name}</td><td>${items.author}</td>`);
-  tr.append($("<td></td>").append(del));
+  // tr.append($("<td></td>").append(del));
   $("#result").append(tr);
 };
 
-var deleteitem = function(){
-  var delval = $(this).val();
-    $.ajax({
-        url:'/api/delete/'+ delval,
-        type:'DELETE',
-        timeout:5000,
-        dataType:'json',
-        success:function(data,textStatus,jqXHR){
-            $("#"+data._id).remove();
-        },
-        error:function(xhr,textStatus){
-            $("#result").html(textStatus)
-        }
-    })
+
+var addTrClass = function(){
+  $(this).addClass(tableclass).siblings().removeClass(tableclass);
 };
 
 
@@ -51,23 +44,23 @@ $(document).ready(function(){
         })
     })
 
-    $("#getAll").click(function(){
-        $.ajax({
-            url:'/api/find',
-            type:'GET',
-            timeout:5000,
-            dataType:'json',
-            success:function(data){
-            $("#result").empty();
-            for (var i = 0; i < data.length; i++) {
-              additem(data[i]);
-            }
-            },
-            error:function(){
-                $("#result").html('0 result')
-            }
-        })
-    })
+    // $("#getAll").click(function(){
+    //     $.ajax({
+    //         url:'/api/find',
+    //         type:'GET',
+    //         timeout:5000,
+    //         dataType:'json',
+    //         success:function(data){
+    //         $("#result").empty();
+    //         for (var i = 0; i < data.length; i++) {
+    //           additem(data[i]);
+    //         }
+    //         },
+    //         error:function(){
+    //             $("#result").html('0 result')
+    //         }
+    //     })
+    // })
 //POST方法
     $("#post").click(function(){
       var data = JSON.stringify({
@@ -93,10 +86,33 @@ $(document).ready(function(){
                 $("#result").html("Error")
             }
         })
-    })
+    });
+
+    $("#delete").click(function(){
+      var delval = $("."+tableclass).attr("value");
+        $.ajax({
+            url:'/api/delete/'+ delval,
+            type:'DELETE',
+            timeout:5000,
+            dataType:'json',
+            success:function(data,textStatus,jqXHR){
+                $("."+tableclass).remove();
+            },
+            error:function(xhr,textStatus){
+                $("#result").html(textStatus)
+            }
+        })
+    });
 
     $('#myModal').on('shown.bs.modal', function() {
         $('#id').focus();
     });
 
+<<<<<<< HEAD
+=======
+    $('.prevent-form').on('submit',function(e){
+      e.preventDefault();
+    });
+
+>>>>>>> 57c01c5... remake delete
 });
